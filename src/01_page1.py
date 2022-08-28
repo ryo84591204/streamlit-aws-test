330e4f7b61970100d74f67480a8834b8c1b503d1
ryo84591204 <chance4u.fate@gmail.com>
2022-08-28 13:07:13 +0900

    Add DataFrame追加
    サンプル用のDataFrameを追加

--- src/01_page1.py
+++ src/01_page1.py
@@ -1,4 +1,13 @@
+import pandas as pd
 import streamlit as st
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
+
+df = pd.DataFrame(
+    data={'列1': [10, 20, 30, 40],
+          '列2': [50, 60, 70, 80],
+          '列3': ['a', 'b', 'c', 'd']}
+)
+
+st.write(df)
